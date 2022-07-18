a72ee7d8cb3739da3286a7364ab61c7776bb9f6a
ChuKien15121998 <chuvankien151298@gmail.com>
2022-07-18 09:28:38 +0700

    build firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
+    firebaseConfig : {
+    apiKey: "AIzaSyDNc_vCQuuTkX8eUPPcqyK0LlWJCRcgLFI",
+    authDomain: "final-project-f1d55.firebaseapp.com",
+    projectId: "final-project-f1d55",
+    storageBucket: "final-project-f1d55.appspot.com",
+    messagingSenderId: "698634315542",
+    appId: "1:698634315542:web:b1850e9d017bf95dff5237",
+    measurementId: "G-SDBQTJY6E6"
+  },
   production: true
 };
