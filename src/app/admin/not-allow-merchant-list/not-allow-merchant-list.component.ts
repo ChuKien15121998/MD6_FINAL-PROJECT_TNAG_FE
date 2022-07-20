1e8a0e550ad34af08665a017779dd0614971652b
au phong <stthu98@gmail.com>
2022-07-21 01:48:13 +0700

    not-allow-template

---
+++ src/app/admin/not-allow-merchant-list/not-allow-merchant-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-not-allow-merchant-list',
+  templateUrl: './not-allow-merchant-list.component.html',
+  styleUrls: ['./not-allow-merchant-list.component.css']
+})
+export class NotAllowMerchantListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
