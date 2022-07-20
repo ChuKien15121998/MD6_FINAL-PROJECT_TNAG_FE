68e1c38e68d22ab72c4ae940631f8ed6aaa2e4f1
au phong <stthu98@gmail.com>
2022-07-20 15:18:11 +0700

    pull dev

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,17 +6,30 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { MerchantRegisterComponent } from './auth/merchant-register/merchant-register.component';
 import {UploadImageComponent} from "./upload/upload-image/upload-image.component";
+import {AngularFireStorageModule} from "@angular/fire/compat/storage";
+import {AngularFireModule} from "@angular/fire/compat";
+import {environment} from "../environments/environment.prod";
+import { CustomerRegisterComponent } from './auth/customer-register/customer-register.component';
+import {MerchantManagementComponent} from "./admin/merchant-management/merchant-management.component";
+import { MerchantLoginComponent } from './auth/merchant-login/merchant-login.component';
+import { CustomerLoginComponent } from './auth/customer-login/customer-login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     MerchantRegisterComponent,
-    UploadImageComponent
+    UploadImageComponent,
+    CustomerRegisterComponent,
+    MerchantManagementComponent,
+    MerchantLoginComponent,
+    CustomerLoginComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFireStorageModule,
+    AngularFireModule.initializeApp(environment.firebaseConfig, "cloud")
   ],
   providers: [],
   bootstrap: [AppComponent]
